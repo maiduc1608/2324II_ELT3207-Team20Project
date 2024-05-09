0374422a8b1ceed04c78ceb58f3cd0acbe796c5c
Phan Thanh Duy <thanhduyphan2123@gmail.com>
2024-05-10 00:27:13 +0700

    update project: send data of DHT11 from STM32 to Labview

--- Read_DHT_data _keil/Read_DHT_data.c
+++ Read_DHT_data _keil/Read_DHT_data.c
@@ -1,11 +1,18 @@
 #include<stm32f10x.h>
+#include<string.h>
+#include<stdio.h>
 
 void SysClkConf(void);
 void SendStart(void);
 void Delay_us(uint32_t);
 void Delay_ms(uint32_t);
 void PinConf(void);
-
+void UART_Conf(void);
+void TIM2_Conf(void);
+void uart_transmit_data(uint8_t*);
+void USART1_IRQHandler(void);
+void convertToFormattedString(double*, char*);
+	
 uint32_t get_us(uint32_t);
 uint8_t ReadDHT(void);
 double	*DHT11_Data(void);
@@ -14,6 +21,9 @@ static uint8_t humidity_integer;
 static uint8_t humidity_decimal;
 static uint8_t temperature_integer;
 static uint8_t temperature_decimal;
+static uint8_t labview_control[2];
+static uint8_t dht_data[5];
+static char DHT_format[9];
 static double temp;
 static double humid;
 static uint8_t checksum;
@@ -21,12 +31,14 @@ static uint8_t checksum;
 
 int main(void){
 	SysClkConf();
+	TIM2_Conf();
 	PinConf();
+	UART_Conf();
 	//wait 1s
 	Delay_ms(1000);
 	
 	while(1){
-		double *data = DHT22_Data();
+		double *data = DHT11_Data();
     temp = data[0];
 		humid = data[1];
 		Delay_ms(1000);
@@ -66,6 +78,11 @@ void SysClkConf(void) {
 	RCC->APB2ENR |= 1<<14 | 1<<2; // en clk for usart1, gpio A
 }
 
+void TIM2_Conf(void){
+	RCC->APB1ENR |= 1;
+	TIM2->PSC = 8-1;
+}
+
 void Delay_us(uint32_t d){
 	uint32_t i;
 	for(i=0;i<d;i++){
@@ -77,14 +94,27 @@ void Delay_us(uint32_t d){
 	}
 }
 
+//void Delay_ms(uint32_t d){
+//	uint32_t i;
+//	for(i=0;i<d;i++){
+//		SysTick->LOAD = 9000-1;
+//		SysTick->VAL = 0;
+//		SysTick->CTRL |= 1<<0;
+//		while(!(SysTick->CTRL & 1<<16));
+//		SysTick->CTRL &= ~(1<<0);
+//	}
+//}
+
 void Delay_ms(uint32_t d){
 	uint32_t i;
 	for(i=0;i<d;i++){
-		SysTick->LOAD = 9000-1;
-		SysTick->VAL = 0;
-		SysTick->CTRL |= 1<<0;
-		while(!(SysTick->CTRL & 1<<16));
-		SysTick->CTRL &= ~(1<<0);
+		TIM2->ARR = 9000;
+		TIM2->CNT = 0;
+		
+		TIM2->CR1 |= 1<<0;
+		while(!(TIM2->SR & 1<<0));
+		TIM2->SR &= ~(TIM_SR_UIF);
+		TIM2->SR &= ~(TIM_SR_UIF);
 	}
 }
 
@@ -129,7 +159,6 @@ uint8_t ReadDHT(void){
 
 double	*DHT22_Data(void){
 	uint8_t i;
-	uint8_t dht_data[5];
 	double DHT22_Data[2];
 	SendStart();
 	for(i=0;i<5;i++){
@@ -145,12 +174,13 @@ double	*DHT22_Data(void){
 		DHT22_Data[0] = (double)(((uint16_t)temperature_integer<<8) + (uint16_t)temperature_decimal)/10.0;
 		DHT22_Data[1] = (double)(((uint16_t)humidity_integer<<8) + (uint16_t)humidity_decimal)/10.0;
 	}
+	convertToFormattedString(DHT22_Data,DHT_format);
+	uart_transmit_data(DHT_format);
 	return DHT22_Data;
 }
 
 double	*DHT11_Data(void){
 	uint8_t i;
-	uint8_t dht_data[5];
 	double DHT11_Data[2];
 	SendStart();
 	for(i=0;i<5;i++){
@@ -162,9 +192,61 @@ double	*DHT11_Data(void){
   temperature_decimal = dht_data[3];
   checksum = dht_data[4];
 	if (checksum == humidity_integer + humidity_decimal + temperature_integer + temperature_decimal){
-		temp = (double)temperature_integer+(double)temperature_decimal/10.0;
-		humid = (double)humidity_integer + (double)humidity_decimal/10.0;
+		DHT11_Data[0] = (double)temperature_integer+(double)temperature_decimal/10.0;
+		DHT11_Data[1] = (double)humidity_integer + (double)humidity_decimal/10.0;
   }
-		
+	convertToFormattedString(DHT11_Data,DHT_format);
+	uart_transmit_data(DHT_format);
 	return DHT11_Data;
+}
+
+void UART_Conf(void){
+	RCC->APB2ENR |= 1<<14 | 1<<2; // en clk for usart1, gpio A
+	GPIOA->CRH &= ~(0xFF<<4);
+	GPIOA->CRH |= 0x8B<<4;
+	//1. enable USART
+	USART1->CR1 |= 1<<13;
+	//2. define word length.
+	USART1->CR1 &= ~(1<<12); // 8bit
+	//3. stop bits
+	USART1->CR2 &= ~(3<<12); //1 stop bit
+	//5. Baud rate
+	USART1->BRR |= 0x1D4C; //115200
+	//6.set TE and bit
+	USART1->CR1 |= 1<<3|1<<2;
+	//set RXNEIE and TXEIE interrupt
+	USART1->CR1 |= 1<<7 | 1<<5;
+}
+void uart_transmit_data(uint8_t *data) {
+  int i = 0;
+  // Loop through each character in the message and transmit
+  while (data[i] != '\0') {
+    // Wait until the transmit data register is empty (TXE flag)
+    while (!(USART1->SR & USART_SR_TXE)) {}
+
+    USART1->DR = data[i++];
+
+    // Wait until the character is transmitted (TC flag)
+    while (!(USART1->SR & USART_SR_TC)) {}
+  }
+}
+
+//void USART1_IRQHandler(void){
+//	if(USART1->SR & 1<<5){
+//		USART1->CR1 &= ~(1<<5);
+//		int i=0;
+//		while(USART1->DR != '\0'){
+//			labview_control[i]=(char)USART1->DR;
+//			i++;
+//			while(!(USART1->SR & 1<<5));
+//		}
+//		USART1->CR1 |= 1<<5;
+//	}
+//}
+
+void convertToFormattedString(double* DHT11_Data, char* output) {
+    int temperature = (int)(DHT11_Data[0] * 10);
+    int humidity = (int)(DHT11_Data[1] * 10);
+
+    sprintf(output, "%04d%04d", temperature, humidity);
 }
