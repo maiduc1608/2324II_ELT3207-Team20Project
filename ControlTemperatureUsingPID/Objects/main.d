4349ee28677f91d8f855696176c23e135449b061
duc mai <maiduc0803@gmail.com>
2024-05-30 13:47:16 +0700

    rename main code

--- ControlTemperatureUsingPID/Objects/main.d
+++ ControlTemperatureUsingPID/Objects/main.d
@@ -1,4 +1,4 @@
-./objects/final_v2.o: final_v2.c \
+./objects/main.o: main.c \
   C:\Users\maidu\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
   RTE\_Target_1\RTE_Components.h \
   C:\Users\maidu\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
